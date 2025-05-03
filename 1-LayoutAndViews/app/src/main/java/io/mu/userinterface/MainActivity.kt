74fca5c3eec7df8c212cd6aad5611e1512c70b7f
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-03 20:36:55 +0300

    feat: Define views in Kotlin code using findViewById

--- 1-LayoutAndViews/app/src/main/java/io/mu/userinterface/MainActivity.kt
+++ 1-LayoutAndViews/app/src/main/java/io/mu/userinterface/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.mu.userinterface
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,15 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        val image = findViewById<ImageView>(R.id.imageView)
+        image.
     }
 }

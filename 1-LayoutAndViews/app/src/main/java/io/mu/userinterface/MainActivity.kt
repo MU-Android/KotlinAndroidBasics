cc90e706b6e7b87f64fb7e68df4aa983bd6cfa89
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-03 20:46:44 +0300

    feat: Introduce View Binding for safer and easier view access

--- 1-LayoutAndViews/app/src/main/java/io/mu/userinterface/MainActivity.kt
+++ 1-LayoutAndViews/app/src/main/java/io/mu/userinterface/MainActivity.kt
@@ -2,23 +2,32 @@ package io.mu.userinterface
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import io.mu.userinterface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         //enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(view)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
 
-        val image = findViewById<ImageView>(R.id.imageView)
-        image.
+        binding.textView.text = "Merhaba Musa"
+
+//        val image = findViewById<ImageView>(R.id.imageView)
+//        val myTextView = findViewById<TextView>(R.id.textView)
+//        myTextView.text = "Merhaba Kotlin"
+//        image.setImageResource(R.drawable.datascience)
     }
 }

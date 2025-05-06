40fd4753d13e4b7c65b3352262488e9ab0b38516
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-07 00:02:02 +0300

    feat: Create RecyclerView item layout for list display

--- 3-RecyclerView/SuperHeroBook/app/src/main/java/io/mu/superherobook/MainActivity.kt
+++ 3-RecyclerView/SuperHeroBook/app/src/main/java/io/mu/superherobook/MainActivity.kt
@@ -9,11 +9,19 @@ import io.mu.superherobook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var superHeroList : ArrayList<SuperHero>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val superman = SuperHero("Superman", "Gazeteci", R.drawable.superman)
+        val batman = SuperHero("Batman", "Patron", R.drawable.batman)
+        val ironman = SuperHero("Iron Man", "Holding Sahibi", R.drawable.ironman)
+        val hulk = SuperHero("Hulk", "Öğrenci", R.drawable.hulk)
+
+        superHeroList = arrayListOf(superman, batman, ironman, hulk)
     }
 }
---
+++ 3-RecyclerView/SuperHeroBook/app/src/main/java/io/mu/superherobook/SuperHero.kt
@@ -0,0 +1,4 @@
+package io.mu.superherobook
+
+class SuperHero(val name : String, val job : String, val image : Int) {
+}

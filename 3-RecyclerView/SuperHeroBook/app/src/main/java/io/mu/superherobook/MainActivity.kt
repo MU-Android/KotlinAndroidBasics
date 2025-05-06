99be310cd80dd61490da3b21ada5fc0d4a446072
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-06 23:44:38 +0300

    feat: Introduce RecyclerView for displaying scrollable lists

---
+++ 3-RecyclerView/SuperHeroBook/app/src/main/java/io/mu/superherobook/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.mu.superherobook
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import io.mu.superherobook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}

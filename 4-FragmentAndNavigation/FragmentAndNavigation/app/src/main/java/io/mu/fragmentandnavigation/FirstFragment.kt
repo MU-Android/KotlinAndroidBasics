7f649531ef8c655d0d596244ea023b0f464ed9fe
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-09 00:17:29 +0300

    feat: Compare lifecycle differences between Activity and Fragment

--- 4-FragmentAndNavigation/FragmentAndNavigation/app/src/main/java/io/mu/fragmentandnavigation/FirstFragment.kt
+++ 4-FragmentAndNavigation/FragmentAndNavigation/app/src/main/java/io/mu/fragmentandnavigation/FirstFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.mu.fragmentandnavigation.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+    private var binding : FragmentFirstBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,6 +17,22 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.editTextName?.setText("")
+        binding?.btnSecondActivity?.setOnClickListener(::next)
+    }
+
+    fun next(view : View?){
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }

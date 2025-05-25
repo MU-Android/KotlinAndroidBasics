7cc8d3883b994449ea435a4c4fa00fa8b53e1bd6
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-05-25 22:53:02 +0300

    feat: Launch activity using ActivityResultLauncher

--- 5-RoomAndDatabase/CookBook/app/src/main/java/io/mu/cookbook/RecipeFragment.kt
+++ 5-RoomAndDatabase/CookBook/app/src/main/java/io/mu/cookbook/RecipeFragment.kt
@@ -1,16 +1,39 @@
 package io.mu.cookbook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import io.mu.cookbook.databinding.FragmentRecipeBinding
-import java.nio.file.Files.delete
 
 class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private var chooseImage: Uri? = null
+    private var chooseBitmap: Bitmap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerLauncher()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -57,5 +80,108 @@ class RecipeFragment : Fragment() {
     }
 
     private fun chooseImage(view: View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    Snackbar.make(
+                        view,
+                        "Galeriye ulaşıp görsel seçmemiz lazım!",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction(
+                        "İzin Ver",
+                        View.OnClickListener {
+
+                        }).show()
+
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                } else {
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            } else {
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    Snackbar.make(
+                        view,
+                        "Galeriye ulaşıp görsel seçmemiz lazım!",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction(
+                        "İzin Ver",
+                        View.OnClickListener {
+
+                        }).show()
+
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                } else {
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            } else {
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
+        }
+
+
+    }
+
+    private fun registerLauncher() {
+
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        chooseImage = intentFromResult.data
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(
+                                    requireActivity().contentResolver,
+                                    chooseImage!!
+                                )
+                                chooseBitmap = ImageDecoder.decodeBitmap(source)
+                            } else {
+                                chooseBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver,
+                                    chooseImage
+                                )
+                            }
+                            binding.imageView.setImageBitmap(chooseBitmap)
+                        } catch (e: Exception) {
+                            println(e)
+                        }
+                    }
+                }
+            }
+
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    Toast.makeText(requireContext(), "İzin verilmedi!", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
